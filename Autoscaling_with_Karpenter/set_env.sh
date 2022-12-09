d7e5732decf04433c8bfe5695787f905a545e730
ChukwuemekaAham <ahamchukwuemeka2@gmail.com>
2022-12-09 08:33:05 +0100

    Updated set_env file

--- Autoscaling_with_Karpenter/set_env.sh
+++ Autoscaling_with_Karpenter/set_env.sh
@@ -5,23 +5,11 @@ export AWS_PROFILE=ahamadmin
 export ACCOUNT_ID=263022081217
 #export AWS_BUCKET=
 export AZS=(us-west-2a us-west-2b us-west-2c us-west-2d)
-export MASTER_ARN=
-export TEST=ok
+# export MASTER_ARN=
 export CLUSTER_NAME=eks-eksctl-demo
 # export ROLE_NAME=
 export KARPENTER_VERSION=v0.19.3
-# export KARPENTER_IAM_ROLE_ARN="arn:aws:iam::263022081217:role/eks-eksctl-demo-karpenter"
-# export ES_DOMAIN_NAME=eks-logging
-# Elasticsearch version
-# export ES_VERSION="OpenSearch_1.0"
-# OpenSearch Dashboards admin user
-# export ES_DOMAIN_USER="adminopensearch"
-# OpenSearch Dashboards admin password
-# export ES_DOMAIN_PASSWORD="$(openssl rand -base64 12)_Ek1$"
-# export ES_ENDPOINT=search-eks-logging-heorfc6pzueheghqboedk3vzca.us-west-2.es.amazonaws.com
-# export FLUENTBIT_ROLE=arn:aws:iam::263022081217:role/eksctl-eks-eksctl-demo-addon-iamserviceaccou-Role1-1QS2PQKHZ9GWD
-#export LBC_VERSION="v2.4.1"
-#export LBC_CHART_VERSION="1.4.1"
+export KARPENTER_IAM_ROLE_ARN="arn:aws:iam::263022081217:role/eks-eksctl-demo-karpenter"
 
 
 
